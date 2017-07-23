7bc524c7597e818736d24c17a5fed0d88602bcf9
hulapoe <rahulchaudhary@RAHULs-MacBook-Pro.local>
2017-07-23 16:27:39 -0400

    Initial version

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,74 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by RAHUL CHAUDHARY on 7/23/17.
+//  Copyright © 2017 hulapoe. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorBrain {
+    var accumulator: Double?
+    
+    var result: Double? {
+        get {
+            return accumulator;
+        }
+    }
+    
+    struct BinaryFunctionOperand {
+        var firstOperand: Double
+        var binaryFunction: (Double,Double) -> Double
+        
+        func performOperation(with secondOperand: Double) -> Double {
+            return binaryFunction(firstOperand,secondOperand)
+        }
+        
+    }
+    
+    var binaryFunctionOperand: BinaryFunctionOperand?
+    
+    enum Operation {
+        case constant(Double)
+        case unaryOperation((Double) -> Double)
+        case binaryOperation((Double,Double) -> Double)
+        case equals
+    }
+    
+    private var operations: Dictionary<String,Operation> = [
+        "π": Operation.constant(Double.pi),
+        "e": Operation.constant(M_E),
+        "√": Operation.unaryOperation(sqrt),
+        "cos": Operation.unaryOperation(cos),
+        "±": Operation.unaryOperation({-$0}),
+        "×": Operation.binaryOperation({$0*$1}),
+        "÷": Operation.binaryOperation({$0/$1}),
+        "+": Operation.binaryOperation({$0+$1}),
+        "-": Operation.binaryOperation({$0-$1}),
+        "=": Operation.equals
+    ]
+    
+    
+    mutating func setOperand(_ operand: Double ) {
+        accumulator = operand
+    }
+    
+    mutating func performOperation(_ symbol: String ) {
+        
+        if let operation = operations[symbol] {
+            switch operation {
+            case .constant(let value):
+                accumulator = value
+            case .unaryOperation(let function):
+                accumulator = function(accumulator!)
+            case .binaryOperation(let binaryFunction):
+                binaryFunctionOperand = BinaryFunctionOperand(firstOperand: accumulator!, binaryFunction: binaryFunction)
+            case .equals:
+                if binaryFunctionOperand != nil && accumulator != nil {
+                    accumulator = binaryFunctionOperand!.performOperation(with: accumulator!)
+                }
+            }
+        }
+    }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var inMiddleOfTyping = false;
+    
+    var brain = CalculatorBrain()
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!;
+        print("\(digit) button touched...");
+        if(inMiddleOfTyping) {
+            let currentDisplayText = display.text!
+            display.text = currentDisplayText + digit;
+        } else {
+            display.text = digit;
+            inMiddleOfTyping = true
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        
+        set {
+            display.text = String(newValue)
+        }
     }
-
-
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        inMiddleOfTyping = false;
+        
+        if let mathematicalOperation = sender.currentTitle  {
+            brain.setOperand(displayValue)
+            brain.performOperation(mathematicalOperation)
+            displayValue = brain.result!;
+        }
+    }
 }
 
